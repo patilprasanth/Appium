d34340b5632c317d065ab5ee394be7dd7be0fd82
patilprasanth <patelprashanth@gmail.com>
2015-05-16 23:10:10 +0530

    Added walnut Manual Expense flow

--- WhatsApp/src/WhatsApp.java
+++ WhatsApp/src/WhatsApp.java
@@ -1,30 +1,64 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
+import java.util.Properties;
+
+import javax.management.openmbean.TabularType;
 
 import io.appium.java_client.android.AndroidDriver;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
 public class WhatsApp 
 {
 	AndroidDriver driver;
+
 	
 	@Test
-	public void open() throws MalformedURLException
+	public void AddingManualExpense() throws MalformedURLException
 	{
 		
 		DesiredCapabilities cap = new DesiredCapabilities();
 		cap.setCapability("deviceName", "ASUS_T00J" );
 		cap.setCapability("platformName", "Android");
 		cap.setCapability("platformVersion","4.4.2");
-		File file = new File("F:\\workspace\\WhatsApp\\Apk\\com.flipkart.android.apk");
+		File file = new File("F:\\Git Repo\\Appium\\WhatsApp\\Apk\\com.daamitt.walnut.app-64.apk");
 		cap.setCapability("app", file.getAbsolutePath());
-		driver = new AndroidDriver(new URL("http://192.168.2.6:4723/wd/hub"), cap);
-		driver.findElement(By.className("android.widget.LinearLayout")).click();
+		driver = new AndroidDriver(new URL("http://192.168.2.2:4723/wd/hub"), cap);
+		driver.findElement(By.id("com.daamitt.walnut.app:id/AddManualTxnFAB")).click();
+		
+		driver.findElement(By.id("com.daamitt.walnut.app:id/FMT1B")).click();
+		driver.findElement(By.id("com.daamitt.walnut.app:id/FMT2B")).click();
+		driver.findElement(By.id("com.daamitt.walnut.app:id/FMT3B")).click();
+		driver.findElement(By.id("com.daamitt.walnut.app:id/FMT4B")).click();
+		driver.findElement(By.id("com.daamitt.walnut.app:id/FMT5B")).click();
+
+		driver.findElement(By.id("com.daamitt.walnut.app:id/FMTFood")).click();
+
+		driver.findElement(By.id("com.daamitt.walnut.app:id/FMTDoneTV")).click();
+		driver.findElement(By.id("com.daamitt.walnut.app:id/monthSpendTotal")).click();
+		String spent_amt = driver.findElement(By.id("com.daamitt.walnut.app:id/STVAmount")).getText();
+		String Amt = (String) spent_amt.subSequence(1,7);
+		String[] finalAmt = Amt.split(",");
+		Amt = finalAmt[0]+finalAmt[1];
+		Assert.assertEquals("12345", Amt);
+		String Cat = driver.findElement(By.id("com.daamitt.walnut.app:id/ASTVCatName")).getText();
+		Assert.assertEquals("Food & Drinks", Cat);
+		spent_amt = driver.findElement(By.id("com.daamitt.walnut.app:id/ASTVAmount")).getText();
+		Amt = (String) spent_amt.subSequence(1,7);
+		finalAmt = Amt.split(",");
+		Amt = finalAmt[0]+finalAmt[1];
+		Assert.assertEquals("12345", Amt);
+		
+		
 	}
 
 }
